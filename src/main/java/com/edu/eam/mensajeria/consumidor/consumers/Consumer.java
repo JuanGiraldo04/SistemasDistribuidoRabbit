b4d1f0dedbb02a531a1cf826a03b9366a84bf6c5
JU4NG1R4L <juanj8845@gmail.com>
2020-10-19 15:02:01 -0500

    Cambios consumer

--- src/main/java/com/edu/eam/mensajeria/consumidor/consumers/Consumer.java
+++ src/main/java/com/edu/eam/mensajeria/consumidor/consumers/Consumer.java
@@ -10,9 +10,11 @@ import com.edu.eam.mensajeria.consumidor.services.CategoryService;
 import com.edu.eam.mensajeria.consumidor.services.ProductService;
 import com.edu.eam.mensajeria.consumidor.services.ProductStoreService;
 import com.edu.eam.mensajeria.consumidor.services.StoreService;
+import org.json.JSONException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.json.JSONArray;
 
 @Component
 public class Consumer {
@@ -34,30 +36,36 @@ public class Consumer {
     @RabbitListener(queues = "#{inventarioQueue.name}")
     public void listenInventarioQueue(String mensaje){
 
-        try{
-            String campos[] = mensaje.split(",");
-            Integer product_id = Integer.parseInt(campos[0]);
-            String product_name = campos[1];
-            String product_price = campos[2];
-            String category_name = campos[3];
-            Integer store_id = Integer.parseInt(campos[5]);
-            String store_name = campos[6];
+        try {
 
-            Category category = categoryService.find(category_name);
-            if(category == null){
-                categoryService.create(new Category(category_name));
-                category = categoryService.find(category_name);
+            JSONArray jsonArray = new JSONArray(mensaje);
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                String campos[] = jsonArray.get(i).toString().split(",");
+                Integer product_id = Integer.parseInt(campos[0]);
+                String product_name = campos[1];
+                String product_price = campos[2];
+                String category_name = campos[3];
+                Integer store_id = Integer.parseInt(campos[5]);
+                String store_name = campos[6];
+
+                Category category = categoryService.find(category_name);
+                if(category == null){
+                    categoryService.create(new Category(category_name));
+                    category = categoryService.find(category_name);
+                }
+
+                productService.create(new Product(product_id, product_name, category.getCategoryId()));
+
+                storeService.create(new Store(store_id, store_name));
+
+                productStoreService.create(new ProductStore(product_id, product_price, store_id));
             }
 
-            productService.create(new Product(product_id, product_name, category.getCategoryId()));
-
-            storeService.create(new Store(store_id, store_name));
-
-            productStoreService.create(new ProductStore(product_id, product_price, store_id));
-
+        } catch (JSONException e) {
+            e.printStackTrace();
         }catch (BusinessException businessException){
             inventarioErrorProducer.produceErrorInventario(mensaje);
         }
-
     }
 }
